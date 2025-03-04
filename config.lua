135f22146840146931857ef758c71ffea0a21470
jua74470 <164792441+jua74470@users.noreply.github.com>
2025-03-04 20:29:32 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 Config = {}
-Config.locale = GetConvar("epyi_administration:locale", "en")
+Config.locale = GetConvar("epyi_administration:locale", "fr")
 
 Config.MenuStyle = {
 	Margins = { left = 10, top = 10 }, -- [table] → Set the menu margins
